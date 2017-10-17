80bea12155b2c9dc2059f6fea0d0f154b8cc539d
Ben Clouser <bclouse91@gmail.com>
2017-10-17 10:31:47 -0700

    WIP jumping machines a bunch, still trying to nail down that image creation

--- tickerCtrl/ImageGen.cpp
+++ tickerCtrl/ImageGen.cpp
@@ -18,6 +18,12 @@ namespace ImageGen{
 
 		pixelCount = testString.len() * 10 * 20;
 
+		// Color of specified text. Obviously make this dynamic later
+		Pixel textColor = {128, 128, 128};
+
+		// Obviously make this dynamic
+		numRows = 20;
+
 		// create the whole container
 		Pixel *newImage = new Pixel [ pixelCount ];
 
@@ -31,9 +37,25 @@ namespace ImageGen{
 				std::cout << "glyph null for char: " << *it << std::endl;
 				continue; 
 			}
-			for(int i = 0; i < glyph->width; ++i){
-				newImage[i].red = textColor.red;
+			if(glyph->height != numRows){
+				std::cout << "Warning: Glyph mismatch. glyph height = " << glyph->height << "image height = " << numRows <<std::endl;
 			}
+
+			for(int row = 0; row < numRows; ++row){
+				for(int i = 0; i < glyph->width; ++i){
+					if(glyph.bitmap[numRows] & 1<<i){
+						newImage[currentWidth*row + i].red = textColor.red;
+						newImage[currentWidth*row + i].green = textColor.green;
+						newImage[currentWidth*row + i].blue = textColor.blue;
+					}
+					else{
+						newImage[currentWidth*row + i].red = 0x00;
+						newImage[currentWidth*row + i].green = 0x00;
+						newImage[currentWidth*row + i].blue = 0x00;
+					}
+				}
+			}
+			current
 
 			glyphs.push_back(glyph);
 			std::cout << "glyph height: " << glyph->height << " glyph width: " << glyph->width <<std::endl;
