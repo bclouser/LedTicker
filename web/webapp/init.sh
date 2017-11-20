95a477b1e3367e6be2dcf8c157b98c9518454b09
Ben Clouser <bclouse91@gmail.com>
2017-11-19 16:21:40 -0800

    Udating init script

--- web/webapp/init.sh
+++ web/webapp/init.sh
@@ -10,3 +10,33 @@
 # db.createUser({user:"someUser", pwd:"somePassword", roles:["readWrite"]})
 
 # db.createCollection("tickerData")
+
+
+mkdir certs
+cd certs
+
+echo "==== Creating signing key"
+openssl genrsa -out server.enc.key 2048 || {
+	echo "Failed to generate signing key"
+	exit -1
+}
+
+echo "==== Creating server request"
+openssl req -new -key server.enc.key -out server.csr || {
+	echo "Failed to create server request \"csr\""
+	exit -1
+}
+
+echo "==== Removing encryption from the private key"
+openssl rsa -in server.enc.key -out server.key || {
+	echo "Failed to strip off encryption of private key"
+	exit -1
+}
+
+echo "==== Self signing the certficate"
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt || {
+	echo "Failed to self-sign the certificate"
+	exit -1
+}
+
+cd ..
